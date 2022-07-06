4b5cd650de999bd07b65f629fa6f8263819e8728
Mitsuya-Sora <72178844+Mitsuya-Sora@users.noreply.github.com>
2022-07-07 02:40:45 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./styles.css";
 import ColorfulMessage from "./components/ColorfulMessage";
+import { useState } from "react";
 
 export default function App() {
-  const onClickButton = () => alert();
+  const [num, countUp] = useState(0);
+  const [isShow, switchIsShow] = useState(false);
+
+  const onClickButton = () => countUp(num + 1);
+
+  if (num > 0 && num % 3 === 0) {
+    isShow || switchIsShow(true);
+  } else {
+    isShow && switchIsShow(false);
+  }
 
   return (
     <div className="App">
@@ -11,7 +21,9 @@ export default function App() {
         color="black"
         message="Start editing to see some magic happen!"
       />
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButton}>カウントアップボタン</button>
+      <p>{num}</p>
+      {isShow && <p>(**)</p>}
     </div>
   );
 }
